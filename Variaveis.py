2e5fd0a0d1b3499fc2a68891499b37fa199eacd9
ka1quegs <102962617+ka1quegs@users.noreply.github.com>
2022-08-17 18:02:48 -0300

    Add files via upload

---
+++ Variaveis.py
@@ -0,0 +1,6 @@
+nome=input("Digite um funcionário:")
+empresa=input("DIgite o nome da empresa:")
+qtde_defuncionarios=int(input("DIgite a quantidade de funcionários:"))
+mediaMensalidade=float(input("Digite a média da mensalidade:"))
+print(nome,"trabalha na", empresa)
+print("Na empresa do mesmo, trabalham", qtde_defuncionarios, "com a media da mensalidade de", mediaMensalidade, "reais")
